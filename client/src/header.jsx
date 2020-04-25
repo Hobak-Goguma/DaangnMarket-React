9b0192fb060ff261a497d046ebd2a812a3292e44
ssul <syhwan88@naver.com>
2020-04-25 20:39:43 +0900

    header 완성&& 메인페이지 사진추가

--- client/src/header.jsx
+++ client/src/header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 const Header = () =>{
     const Headers = styled.header`
-        border-bottom: 1px solid #ccc;
+        box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.1);
+        
         .container{
             position: relative;
             width: 980px;
             margin : 0 auto;
             img{
+                display:inline-block;
+                margin-bottom:15px;
                 padding : 10px 0;
                 width: 132px;
             }
@@ -15,14 +18,25 @@ const Header = () =>{
                 display:inline-block;
             }
             .inputBox{
-                padding :10px;
-                border: 1px solid #ccc;
+                margin:20px 0 20px 20px;
+                width: 470px;
+                height:20px;
+                position:relative;
+                padding :12px;
+                border: 1px solid #FF8A3D;
                 border-radius:5px;
                 input{
-                    font-size:20px;
+                    position: absolute;
+                    top: 13px;
+                    left:10px;
+                    width:440px;
+                    font-size:17px;
                     border:0;
                 }
                 label{
+                    position:absolute;
+                    top:10px;
+                    right:10px;
                     img{
                         padding : 0;
                         width: 26px;
@@ -35,12 +49,18 @@ const Header = () =>{
                 right :0;
                 transform: translateY(-50%);
                 div{
-                    width: 150px;
-                    border:1px solid #ccc;
-                    border-radius:25px;
+                    width: 140px;
+                    border:1px solid #FF8A3D;
+                    border-radius:20px;
+                    color:#FF8A3D;
+                    font-weight:bold;
                     text-align:center;
-                    height:50px;
+                    height:40px;
                     position:relative;
+                    &:hover{
+                        background-color:#FF8A3D;
+                        color:white;
+                    }
                     span{
                         display:block;
                         position:absolute;
@@ -60,7 +80,7 @@ const Header = () =>{
         <div className="container">
             <img src="./img/logo.svg" alt="로고"/>
             <div className="inputBox">
-                <input type="text" name="search" id="searchItem"/>
+                <input type="text" name="search" id="searchItem" placeholder="지역, 상품, 업체등을 검색해보세요."/>
                 <label htmlFor="searchItem">
                     <img src="./img/search-icon.svg" alt="search"/>
                 </label>
