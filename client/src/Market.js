64a73ce670d585b20c96b509129620d47647c2ae
ssul <syhwan88@naver.com>
2020-04-30 10:41:40 +0900

    [영환] 4/30

--- client/src/Market.js
+++ client/src/Market.js
@@ -1,98 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Market = () =>{
-    const items=[{
-        name: "접이식자전거",
-        location: "인천 연수구 송도동",
-        thumb:"./img/접이식자전거.jpg",
-        cost: 20000,
-        like: 5,
-        chat: 40
-    },{
-        name: "캠핑 테이블",
-        location: "경기도 시흥시 능곡동",
-        thumb:"./img/캠핑 테이블.jpg",
-        cost: 5000,
-        like: 5,
-        chat: 25
-    },{
-        name: "TV모니터 32인치",
-        location: "경기도 고양시 일산서구 주엽동",
-        thumb:"./img/TV모니터 32인치.jpg",
-        cost: 15000,
-        like: 5,
-        chat: 28
-    },{
-        name: "창고정리 예초기 전기톱",
-        location: "서귀포시 서귀동",
-        thumb:"./img/예초기.jpg",
-        cost: 60000,
-        like: 3,
-        chat: 30
-    },{
-        name: "4단 선반",
-        location: "제주 제주시 연동",
-        thumb:"./img/4단선반.jpg",
-        cost: 25000,
-        like: 23,
-        chat: 35
-    },{
-        name: "전자렌지",
-        location: "울산 중구 우정동",
-        thumb:"./img/전자렌지.jpg",
-        cost: 20000,
-        like: 6,
-        chat: 34
-    },{
-        name: "김치냉장고",
-        location: "제주 제주시 삼도1동",
-        thumb:"./img/김치냉장고.jpg",
-        cost: 100000,
-        like: 2,
-        chat: 29
-    },{
-        name: "싱거 재봉틀 싱거미싱기",
-        location: "서울 송파구 잠실3동",
-        thumb:"./img/미싱.jpg",
-        cost: 25000,
-        like: 14,
-        chat: 24
-    },{
-        name: "화분무료드림",
-        location: "경기도 구리시 갈매동",
-        thumb:"./img/화분.jpg",
-        cost: 0,
-        like: 3,
-        chat: 11
-    },]
-    const Items = items.map(v=>{
-        let cost = "";
-        const costString=String(v.cost);
-        for(let i = 0 ; i <costString.length;i++){
-            cost += costString[i];
-            if((costString.length-1-i)%3===0 &&costString.length-1 !== i){
-                cost+=",";
-            }
-        }
 
-        return(
-        <div className="item" key = {v.name}>
-            <div className="img">
-                <img src={v.thumb} alt={v.name}/>
-            </div>
-            <div className="discription">    
-                <h1>{v.name}</h1>
-                <p>{v.location}</p>
-                <p className="price">{v.cost!==0? 
-                cost+"원"
-                :"무료"}</p>
-            </div>
-            <div className="like">
-                <span>{`관심 ${v.like} ∙ 채팅 ${v.chat}`}</span>
-            </div>
-        </div>);
-    });
 const Section = styled.section`
 margin-top:150px;
 .container{
@@ -195,6 +104,100 @@ margin-top:150px;
     }
 }
 `;
+
+const Market = () =>{
+    const items=[{
+        name: "접이식자전거",
+        location: "인천 연수구 송도동",
+        thumb:"./img/접이식자전거.jpg",
+        cost: 20000,
+        like: 5,
+        chat: 40
+    },{
+        name: "캠핑 테이블",
+        location: "경기도 시흥시 능곡동",
+        thumb:"./img/캠핑 테이블.jpg",
+        cost: 5000,
+        like: 5,
+        chat: 25
+    },{
+        name: "TV모니터 32인치",
+        location: "경기도 고양시 일산서구 주엽동",
+        thumb:"./img/TV모니터 32인치.jpg",
+        cost: 15000,
+        like: 5,
+        chat: 28
+    },{
+        name: "창고정리 예초기 전기톱",
+        location: "서귀포시 서귀동",
+        thumb:"./img/예초기.jpg",
+        cost: 60000,
+        like: 3,
+        chat: 30
+    },{
+        name: "4단 선반",
+        location: "제주 제주시 연동",
+        thumb:"./img/4단선반.jpg",
+        cost: 25000,
+        like: 23,
+        chat: 35
+    },{
+        name: "전자렌지",
+        location: "울산 중구 우정동",
+        thumb:"./img/전자렌지.jpg",
+        cost: 20000,
+        like: 6,
+        chat: 34
+    },{
+        name: "김치냉장고",
+        location: "제주 제주시 삼도1동",
+        thumb:"./img/김치냉장고.jpg",
+        cost: 100000,
+        like: 2,
+        chat: 29
+    },{
+        name: "싱거 재봉틀 싱거미싱기",
+        location: "서울 송파구 잠실3동",
+        thumb:"./img/미싱.jpg",
+        cost: 25000,
+        like: 14,
+        chat: 24
+    },{
+        name: "화분무료드림",
+        location: "경기도 구리시 갈매동",
+        thumb:"./img/화분.jpg",
+        cost: 0,
+        like: 3,
+        chat: 11
+    },]
+    const Items = items.map(v=>{
+        let cost = "";
+        const costString=String(v.cost);
+        for(let i = 0 ; i <costString.length;i++){
+            cost += costString[i];
+            if((costString.length-1-i)%3===0 &&costString.length-1 !== i){
+                cost+=",";
+            }
+        }
+
+        return(
+        <div className="item" key = {v.name}>
+            <div className="img">
+                <img src={v.thumb} alt={v.name}/>
+            </div>
+            <div className="discription">    
+                <h1>{v.name}</h1>
+                <p>{v.location}</p>
+                <p className="price">{v.cost!==0? 
+                cost+"원"
+                :"무료"}</p>
+            </div>
+            <div className="like">
+                <span>{`관심 ${v.like} ∙ 채팅 ${v.chat}`}</span>
+            </div>
+        </div>);
+    });
+
 
     return(
     <Section>
