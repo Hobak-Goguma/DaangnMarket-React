546c3bfc61b746ea786a4d5595ab915a8c63a75b
ssul <syhwan88@naver.com>
2020-04-30 10:33:42 +0900

    [4/30] 영환

--- client/src/Market.js
+++ client/src/Market.js
@@ -67,9 +67,30 @@ const Market = () =>{
         chat: 11
     },]
     const Items = items.map(v=>{
+        let cost = "";
+        const costString=String(v.cost);
+        for(let i = 0 ; i <costString.length;i++){
+            cost += costString[i];
+            if((costString.length-1-i)%3===0 &&costString.length-1 !== i){
+                cost+=",";
+            }
+        }
+
         return(
-        <div className="item">
-            <img src={v.thumb} alt={v.name}/>
+        <div className="item" key = {v.name}>
+            <div className="img">
+                <img src={v.thumb} alt={v.name}/>
+            </div>
+            <div className="discription">    
+                <h1>{v.name}</h1>
+                <p>{v.location}</p>
+                <p className="price">{v.cost!==0? 
+                cost+"원"
+                :"무료"}</p>
+            </div>
+            <div className="like">
+                <span>{`관심 ${v.like} ∙ 채팅 ${v.chat}`}</span>
+            </div>
         </div>);
     });
 const Section = styled.section`
@@ -97,22 +118,80 @@ margin-top:150px;
     section{
         margin: 0 auto;
         text-align:center;
-        .item{
+        >div{
+            position:relative;
             display: inline-block;
             width: 20%;
-            height:40vh;
-            background-color: red;
+            height:317px;
+            border:1px solid #ddd;
             margin: 0 50px 50px 0;
             border-radius:20px;
             overflow:hidden;
+            &:hover{
+                box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+                -webkit-box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+                -moz-box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2);
+                top: -2px;
+            }
+        }   
+        
+        .item{
             &:nth-child(4n){
                 margin-right:0;
             }
-            img{
+            .img{
                 width:100%;
+                height:50%;
+                overflow:hidden;
+                img{
+                    width:100%;
+                }
+            }
+            .discription{
+                margin:20px;
+                position:relative;
+                h1{
+                    margin-top:20px;
+                    font-size:18px;
+                    margin-top:7px;
+                    white-space : nowrap; 
+                    text-overflow : ellipsis;
+                    overflow:hidden;
+                }   
+                p{ 
+                    text-align:left;
+                    font-size:15px;
+                    margin-top:7px;
+                    white-space : nowrap; 
+                    text-overflow : ellipsis;
+                    overflow:hidden;
+                }
+                .price{
+                    color:#FF8A3D;
+                }
+            }
+            .like{
+                margin:-5px 10px 5px 10px;
+                height:35px;
+                border-top:1px solid #ddd;
+                text-align:left;
+                span{
+                    display:block;
+                    margin-top:12px;
+                    font-size:13px;
+                    color:#bbb;
+                }
             }
         }
-        
+        .more{
+            box-sizing:border-box;
+            margin-right:0;
+            padding: 88px 0;
+            div{
+                font-size:20px;
+                margin: 22px 20px 0 20px;
+            }
+        }
     }
 }
 `;
@@ -123,6 +202,10 @@ margin-top:150px;
             <div className="title">당근마켓 인기 매물</div>
             <section>
                 {Items}
+                <div className="more">
+                    <img src="https://d1unjqcospf8gs.cloudfront.net/assets/home/base/mobile/icon-add-dcfb7aac98f18d19ca4c27e9dc29c63cf73861f3b10e0a42c06163d2658d0691.svg" alt="더보기"/>
+                    <div>당근마켓<br/>인기매물 더보기</div>
+                </div>
             </section>
         </div>
     </Section>);
