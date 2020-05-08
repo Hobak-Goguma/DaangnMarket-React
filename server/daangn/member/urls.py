12c2a41fb7816826bf22eee39e0af03e47fe77fd
ddusi <58924238+ddusi@users.noreply.github.com>
2020-05-08 23:15:40 +0900

    로그인 api 구현 성공

---
+++ server/daangn/member/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from member import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('', views.member_list, name='member'),
+    path('login/', views.member_login, name='login'),
+    path('<pk>/', views.member_detail, name='member'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/daangn/member/views.py
+++ server/daangn/member/views.py
@@ -1,3 +1,85 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from member.models import Member
+from member.serializers import *
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def member_list(request):
+    """
+    코드 조각을 모두 보여주거나 새 코드 조각을 만듭니다.
+    """
+    if request.method == 'GET':
+        member = Member.objects.all()
+        serializer = MemberSerializer(member, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MemberSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def member_login(request):
+    """
+    멤버 테이블 로그인
+    """
+    try:
+        Data = json.loads(request.body)
+        user_id = Data['user_id']
+        user_pw = Data['user_pw']
+        member = Member.objects.get(user_id = user_id, user_pw = user_pw)
+    except Member.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+        # return HttpResponse(data)
+
+    if request.method == 'POST':
+        serializer = LoginSerializer(member)
+        return Response(serializer.data)
+
+    # user_pw = Data['user_pw']
+    # if Member.user_pw == user_pw:
+    #     if request.method == 'POST':
+    #         serializer = LoginSerializer(member)
+    #         return Response(serializer.data)
+    # else:
+    #     return HttpResponse('비밀번호가 틀렸습니다.')
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def member_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        member = Member.objects.get(pk=pk)
+    except Member.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MemberSerializer(member)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MemberSerializer(member, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        Member.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+    
