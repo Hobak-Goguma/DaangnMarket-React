681bdce3e3e23c95a11f7c89d0d772e46d99bc37
ddusi <58924238+ddusi@users.noreply.github.com>
2020-05-16 16:49:33 +0900

    루트 member/ 디렉토리 삭제. member와 product URL분리

--- server/daangn/member/urls.py
+++ server/daangn/member/urls.py
@@ -3,14 +3,14 @@ from member import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', views.member_list, name='member_list'),
-    path('login/', views.member_login, name='login'),
-    path('product/search/', views.product_search, name='prduct_search'),
+    path('member/', views.member_list, name='member_list'),
+    path('member/login/', views.member_login, name='login'),
+    path('member/overlap/', views.member_overlap, name='overlap'),
+    path('member/<pk>/', views.member_detail, name='member_detail'),
     path('product/', views.product_list, name='prduct_list'),
     path('product/<pk>', views.product_detail, name='prduct_detail'),
-    path('overlap/', views.member_overlap, name='overlap'),
+    path('product/search/', views.product_search, name='prduct_search'),
     path('test/', views.test, name='test'),
-    path('<pk>/', views.member_detail, name='member_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
