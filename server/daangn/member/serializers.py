7b95788f5fb917a9714a6579377bdaad4f9c6d3e
ddusi <58924238+ddusi@users.noreply.github.com>
2020-05-28 21:57:39 +0900

    깃 이그노어를 위한 수정

---
+++ server/daangn/member/serializers.py
@@ -0,0 +1,112 @@
+from django.forms import widgets
+from rest_framework import serializers
+from member.models import *
+# from member.serializers import *
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Member
+        # fields = ('pk', 'name', 'nick_nazme', 'user_id', 'user_pw', 'tel', 'birth', 'email', 'gender')
+        fields = ('pk', 'name', 'nick_name', 'user_id', 'user_pw', 'tel', 'birth', 'email', 'gender', 'addr', 'img')
+
+
+class MemberReviseSerializer(serializers.ModelSerializer):
+    udate = serializers.DateTimeField(default=timezone.now)
+    class Meta:
+        model = Member
+        fields = ('pk', 'user_pw', 'udate')
+
+
+class MemberTouchSerializer(serializers.ModelSerializer):
+    udate = serializers.DateTimeField(default=timezone.now)
+    addr = serializers.JSONField()
+
+    class Meta:
+        model = Member
+        fields = ('pk', 'nick_name', 'tel', 'birth', 'email', 'addr', 'img', 'udate')
+        
+class ProductSerializer(serializers.ModelSerializer):
+    # id_member_id = serializers.IntegerField(source='id_member')
+    # member = serializers.ForeignKey(Member, models.CASCADE, related_name='member_id')
+    class Meta:
+        model = Product
+        fields = ('pk', 'id_member', 'name', 'price', 'info', 'category', 'img')
+
+class LoginSerializer(serializers.ModelSerializer):
+    last_date = serializers.DateTimeField(default=timezone.now)
+    class Meta:
+        model = Member
+        fields = ('pk','user_id', 'user_pw', 'name', 'nick_name', 'tel', 'addr', 'last_date', 'img')
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('pk', 'id_member', 'name', 'addr', 'tel', 'info', 'category', 'img')
+
+class WishlistSerializer(serializers.ModelSerializer):
+    id_member = MemberSerializer(read_only=True)
+    id_product = ProductSerializer(read_only=True)
+    class Meta:
+        model = Wishlist
+        fields = ('pk', 'id_product', 'id_member', 'cdate')
+
+
+# realdeal api 
+class RealDealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ('pk', 'id_product', 'cdate')
+
+class MemberSellerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ('id_member', 'id_real_deal')
+
+class MemberShopperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ('id_member', 'id_real_deal')
+
+class SellerReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ('pk', 'id_real_deal', 'title', 'cdate')
+
+class ShopperReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ('pk', 'id_real_deal', 'title', 'cdate')
+
+
+# class MemberSerializer(serializers.Serializer):
+#     id_member = serializers.AutoField(primary_key=True)
+#     name = serializers.CharField(max_length=30)
+#     nick_name = serializers.CharField(max_length=30)
+#     user_id = serializers.CharField(max_length=30)
+#     user_pw = serializers.CharField(max_length=55)
+#     tel = serializers.CharField(max_length=20)
+#     birth = serializers.DateField()
+#     email = serializers.CharField(max_length=30)
+#     gender = serializers.CharField(max_length=6)
+#     add = serializers.CharField(max_length=200)
+#     cdate = serializers.DateTimeField()
+#     udate = serializers.DateTimeField()
+#     last_date = serializers.DateTimeField()
+
+#     def create(self, validated_data):
+#         """
+#         검증한 데이터로 새 `Member` 인스턴스를 생성하여 리턴합니다.
+#         """
+#         return Member.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data):
+#         """
+#         검증한 데이터로 기존 `Member` 인스턴스를 업데이트한 후 리턴합니다.
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
---
+++ server/daangn/member/views.py
@@ -0,0 +1,363 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from member.models import *
+from member.serializers import *
+from django.http import HttpResponse
+from django.utils import timezone
+import json
+from django.http import JsonResponse
+from drf_yasg.utils import swagger_auto_schema
+
+now = timezone.now()
+
+# 'method' can be used to customize a single HTTP method of a view
+@swagger_auto_schema(method='get', responses={200:'OK'})
+# 'methods' can be used to apply the same modification to multiple methods
+@swagger_auto_schema(methods=['post'], request_body=MemberSerializer)
+@api_view(['GET', 'POST'])
+def member_list(request):
+    """
+    모든 유저 조회, 유저 등록
+    ---
+    모든 유저의 정보를 보여주거나 새 유저 정보를 등록합니다.
+    """
+    
+    if request.method == 'GET':
+        member = Member.objects.all()
+        serializer = MemberSerializer(member, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = MemberSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def member_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        member = Member.objects.get(pk=pk)
+    except Member.DoesNotExist:
+        content = {
+            "message" : "없는 사용자 입니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MemberSerializer(member)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = MemberReviseSerializer(member, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        member.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['PUT'])
+def member_touch(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        member = Member.objects.get(pk=pk)
+    except Member.DoesNotExist:
+        content = {
+            "message" : "없는 사용자 입니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = MemberTouchSerializer(member, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def member_search(request):
+    """
+    특정유저를 아이디로 검색합니다.
+    """
+    try:
+        User_id = request.GET['user_id']
+        member = Member.objects.get(user_id = User_id)
+    except Member.DoesNotExist:
+        content = {
+            "message" : "유저를 찾을 수 없습니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = MemberSerializer(member)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+def member_overlap(request):
+    """
+    아이디값이 중복되는지 확인해줍니다.
+    """
+    try:
+        User_id = request.GET['user_id']
+        member = Member.objects.get(user_id = User_id)
+    except Member.DoesNotExist:
+        content = {
+            "message" : "중복아이디가 없습니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_200_OK)
+        
+    content = {
+            "message" : "중복아이디가 있습니다.",
+            "result" : {}
+                }
+    return Response(content, status=status.HTTP_409_CONFLICT)
+
+@api_view(['GET'])
+def nick_name_overlap(request):
+    """
+    닉네임 값이 중복되는지 확인해줍니다.
+    """
+    try:
+        Nick_name = request.GET['nick_name']
+        member = Member.objects.get(nick_name = Nick_name)
+    except Member.DoesNotExist:
+        content = {
+            "message" : "중복닉네임이 없습니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_200_OK)
+        
+    content = {
+            "message" : "중복닉네임이 있습니다.",
+            "result" : {}
+                }
+    return Response(content, status=status.HTTP_409_CONFLICT)
+
+@api_view(['POST'])
+def member_login(request):
+    """
+    멤버 테이블 로그인
+    """
+    try:
+        Data = json.loads(request.body)
+        user_id = Data['user_id']
+        user_pw = Data['user_pw']
+        member = Member.objects.get(user_id = user_id, user_pw = user_pw)
+
+    except Member.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # if request.method == 'POST':
+    #     serializer = LoginSerializer(member, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #     return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = LoginSerializer(member)
+        return Response(serializer.data)
+
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    """
+    상품을 모두 보여주거나 새 상품리스트를 만듭니다.
+    """
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    """
+    코드 조각 조회, 업데이트, 삭제
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        content = {
+            "message" : "없는 물품리스트 입니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        Product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    
+@api_view(['GET'])
+def product_search(request):
+    """
+    제목에 검색어가 포함된 물건들 리스트
+    """
+    try:
+        Search = request.GET['q']
+        product = Product.objects.filter(name__contains = Search)
+        # product = Product.objects.get(name = Search)
+    except Product.DoesNotExist:
+        #검색 결과 없음.
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    #검색결과 있음.
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data)
+    # return HttpResponse(product)
+
+
+@api_view(['GET'])
+def product_category(request):
+    """
+    제목에 검색어가 포함된 물건들 리스트
+    """
+    Search = request.GET['q']
+    product = Product.objects.filter(category = Search)
+    
+    if not product:
+        #검색 결과 없음.
+        content = {
+            "message" : "잘못된 카테고리 입니다.",
+            "result" : {"입력한 검색어" : Search}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+    else:
+        #검색결과 있음.
+        serializer = ProductSerializer(product, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def company_list(request):
+    """
+    업체리스트를 모두 보여주거나 새 업체리스트를 만듭니다.
+    """
+    if request.method == 'GET':
+        company = Company.objects.all()
+        serializer = CompanySerializer(company, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def company_detail(request, pk):
+    """
+    특정 업체리스트를 조회, 수정, 삭제 합니다.
+    """
+    try:
+        company = Company.objects.get(pk=pk)
+    except Company.DoesNotExist:
+        content = {
+            "message" : "없는 업체 입니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = CompanySerializer(company, data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        Company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'POST'])
+def wishlist_list(request):
+    """
+    찜리스트를 모두 보여주거나 새 찜을 추가합니다.
+    """
+    if request.method == 'GET':
+        wishlist = Wishlist.objects.all()
+        serializer = WishlistSerializer(wishlist, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = WishlistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'DELETE'])
+def wishlist_detail(request, pk):
+    """
+    특정 유저의 찜리스트를 조회, 삭제 합니다.
+    """
+    try:
+        wishlist = Wishlist.objects.filter(id_member = pk)
+    except Wishlist.DoesNotExist:
+        content = {
+            "message" : "찜한 상품이 없습니다.",
+            "result" : {}
+                }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = WishlistSerializer(wishlist, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        wishlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def test(request):
+    """
+    테스트용 api
+    """
+    return Response(status=status.HTTP_200_OK)
