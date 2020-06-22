5d7fc6bb7e22e9eae383c5f9eca590e9fde098c8
soheeyun831 <sul831@naver.com>
2020-06-22 21:37:58 +0900

    [소희] Card component : style 수정  - 조금 더 공통 컴포넌트처럼 메인에서만 사용할 요소는 메인페이지 안으로 이동

--- client/src/components/Card/Item/index.js
+++ client/src/components/Card/Item/index.js
@@ -22,10 +22,11 @@ const CardItemArticle = styled.div`
   &:nth-child(-n + 4){
       margin-top:0;
   }
-.card-item{
-width: 100%;
-height: 100%;
-position: relative;
+  .card-item{
+  width: 100%;
+  height: 100%;
+  position: relative;
+  cursor: pointer;
   .img{
       width:100%;
       height:50%;
@@ -81,35 +82,35 @@ position: relative;
 `;
 
 const CardItem = (props) => {
-	let cost = "";
-	const costString = String(props.item.cost);
-	for (let i = 0; i < costString.length; i++) { // 3자리마다 , 넣기위하여
-		cost += costString[i];
-		if ((costString.length - 1 - i) % 3 === 0 && costString.length - 1
-			!== i) {
-			cost += ",";
-		}
-	}
-	return (
-		<CardItemArticle>
-			<div className="card-item" key={props.item.name}>
-				<div className="img">
-					<img src={props.item.thumb} alt={props.item.name}/>
-				</div>
-				<div className="text-group">
-					<div className="description">
-						<h1>{props.item.name}</h1>
-						<p>{props.item.location}</p>
-						<p className="price">{props.item.cost !== 0 ?
-							cost + "원"
-							: "무료"}</p>
-					</div>
-					<div className="like">
-						<span>{`관심 ${props.item.like} ∙ 채팅 ${props.item.chat}`}</span>
-					</div>
-				</div>
-			</div>
-		</CardItemArticle>
-	);
+  let cost = "";
+  const costString = String(props.item.cost);
+  for (let i = 0; i < costString.length; i++) { // 3자리마다 , 넣기위하여
+    cost += costString[i];
+    if ((costString.length - 1 - i) % 3 === 0 && costString.length - 1
+        !== i) {
+      cost += ",";
+    }
+  }
+  return (
+      <CardItemArticle>
+        <div className="card-item" key={props.item.name}>
+          <div className="img">
+            <img src={props.item.thumb} alt={props.item.name}/>
+          </div>
+          <div className="text-group">
+            <div className="description">
+              <h1>{props.item.name}</h1>
+              <p>{props.item.location}</p>
+              <p className="price">{props.item.cost !== 0 ?
+                  cost + "원"
+                  : "무료"}</p>
+            </div>
+            <div className="like">
+              <span>{`관심 ${props.item.like} ∙ 채팅 ${props.item.chat}`}</span>
+            </div>
+          </div>
+        </div>
+      </CardItemArticle>
+  );
 };
 export default CardItem;
