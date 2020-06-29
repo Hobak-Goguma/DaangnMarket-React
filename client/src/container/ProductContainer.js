33c92df1b3ef8f8623d3f3d98dafcb8a1e4ce2b0
yhsul <yhsul@nshc.net>
2020-06-29 17:02:50 +0900

    [영환] 제품상세페이지 수정
     - 컴포넌트에서 container로 병합
     - wishlist footer 를 component로 따로 폴더 생성

--- client/src/container/ProductContainer.js
+++ client/src/container/ProductContainer.js
@@ -1,15 +1,46 @@
-import React, { useState } from "react";
-import ProductDetail from "../components/ProductDetail";
+import styled from "styled-components";
+import React, { useState,useEffect } from "react";
+import api from "../common/api";
 import Layout from "../common/components/Layout";
+import Slider from "../components/Slider";
+import DetailHeader from "../components/DetailHeader";
+import Description from "../components/DetailDescription";
+
+
+const ArticleWrapper = styled.div`
+  margin-top: 120px;
+  padding-bottom: 50px;
+`;
+
+
 
 const ProductDetailContainer = () => {
+  let id = window.location.href.substring(window.location.href.indexOf("products")+9);
   const [isModal, setModal] = useState(false);
+  const [sliderData, setSliderData] = useState([]);
+  const [detail, setDetail] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data/sliderData.json")
+      .then((res) => res.json())
+      .then((res) => {
+        setSliderData(res.sliderData1);
+      });
+    api.get(`/product/${id}`).then((res) => setDetail(res.data));
+  }, []);
+
+
   return (
     <Layout
       overflow={isModal ? "hidden" : "unset"}
       height={isModal ? "100vh" : "unset"}
     >
-      <ProductDetail isModal={isModal} setModal={setModal} />
+      {/* <ProductDetail isModal={isModal} setModal={setModal} /> */}
+      <ArticleWrapper>
+        <Slider sliderData={sliderData} isModal={isModal} setModal={setModal} />
+        <DetailHeader />
+        <Description detail={detail} />
+      </ArticleWrapper>
     </Layout>
   );
 };
