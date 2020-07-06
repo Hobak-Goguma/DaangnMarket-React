92aa215677ad31692ead81344e4b8cfc55e5b502
DevLeeSeonwoo <leeseonwoo5305@gmail.com>
2020-07-06 13:22:19 +0900

    [seonwoo] DB 20200706 Member addr add column distance & select

--- server/daangn/member/models.py
+++ server/daangn/member/models.py
@@ -108,6 +108,8 @@ class Memberaddr(models.Model):
     id_member_addr = models.AutoField(primary_key=True)
     id_member = models.ForeignKey(Member, models.DO_NOTHING, db_column='id_member')
     addr = models.CharField(max_length=200)
+    distance = models.IntegerField(default=0)
+    select = models.CharField(max_length=8, default="Y")
 
     class Meta:
         managed = False
